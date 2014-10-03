c4c6ecc7d76204d7226af0dfac6de8ced34b1bb9
Chris Droukas <c.droukas@gmail.com>
2014-10-03 16:00:38 -0400

    Modify appdelegate to show webkit controller

--- ARTemplating/ARAppDelegate.m
+++ ARTemplating/ARAppDelegate.m
@@ -9,6 +9,7 @@
 #import "ARAppDelegate.h"
 
 #import "ARViewController.h"
+#import "ARWKViewController.h"
 #import "ARNativeTemplateViewController.h"
 
 @implementation ARAppDelegate
@@ -16,13 +17,15 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     UIViewController *jsVC = [[ARViewController alloc] initWithNibName:@"ARViewController" bundle:nil];
+    UIViewController *wkVC = [[ARWKViewController alloc] initWithNibName:@"ARWKViewController" bundle:nil];
     UIViewController *grVC = [[ARNativeTemplateViewController alloc] initWithNibName:@"ARNativeTemplateViewController" bundle:nil];
-
-    jsVC.title = @"JSMustache";
+    
+    jsVC.title = @"JSMustache (UIWebView)";
+    wkVC.title = @"JSMustache (WKWebView)";
     grVC.title = @"GRMustache";
-
+    
     self.tabBarController = [[UITabBarController alloc] init];
-    self.tabBarController.viewControllers = @[jsVC, grVC];
+    self.tabBarController.viewControllers = @[jsVC, wkVC, grVC];
     self.window.rootViewController = self.tabBarController;
     [self.window makeKeyAndVisible];
     return YES;
